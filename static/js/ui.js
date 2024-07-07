bdbb2048cf631a81dae2fe1c35b2a14e91dcb672
KokoPrograms <65005317+KokoPrograms@users.noreply.github.com>
2024-07-07 12:48:35 -0600

    segment container ui tweaks

--- static/js/ui.js
+++ static/js/ui.js
@@ -8,33 +8,37 @@ function displaySegmentedTranscription(segments, isSorted = true) {
     
     segments.forEach((segment, index) => {
         const div = document.createElement('div');
-        div.className = 'mb-2 px-4 py-2 bg-white rounded-lg cursor-pointer transition duration-300 ease-in-out segment-container';
+        div.className = 'mb-2 px-4 pt-2 pb-1 bg-white rounded-lg cursor-pointer transition duration-300 ease-in-out segment-container';
         div.setAttribute('data-index', index);
         const duration = (segment.end - segment.start).toFixed(2);
         div.innerHTML = `
-            <textarea class="w-full p-2 border border-slate-300 rounded-md focus:outline-none focus:ring-0 segment-text" data-index="${index}" rows="2">${segment.text}</textarea>
-            <div class="mt-1 flex justify-between items-center">
-                <button class="remove-segment p-1 text-red-500 hover:text-red-700 rounded-full hover:bg-red-100 transition duration-300 ease-in-out" data-index="${index}" title="Delete segment">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                    </svg>
-                </button>
-                <span class="text-sm text-slate-600 segment-time">${duration}s</span>
-                <div class="flex items-center space-x-4">
-                    <input type="number" class="segment-start w-20 p-1 border border-slate-300 rounded-md focus:outline-none focus:ring-0" value="${segment.start.toFixed(2)}" step="0.01" min="0" data-index="${index}">
-                    <span class="text-sm text-slate-600">to</span>
-                    <input type="number" class="segment-end w-20 p-1 border border-slate-300 rounded-md focus:outline-none focus:ring-0" value="${segment.end.toFixed(2)}" step="0.01" min="0" data-index="${index}">
+            <div class="flex justify-between items-start">
+                <div class="flex-grow pr-4">
+                <textarea class="w-full p-2 border border-slate-300 rounded-md focus:outline-none focus:ring-0 segment-text h-[4.5em] overflow-y-auto" data-index="${index}">${segment.text}</textarea>
+                    <div class="px-3 flex justify-between items-center w-full">
+                        <button class="remove-segment p-1 text-red-500 hover:text-red-700 rounded-full hover:bg-red-100 transition duration-300 ease-in-out" data-index="${index}" title="Delete segment">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                            </svg>
+                        </button>
+                        <div class="flex-grow"></div>
+                        <button class="retranscribe-segment p-1 text-green-500 hover:text-green-700 rounded-full hover:bg-green-100 transition duration-300 ease-in-out" data-index="${index}" title="Re-transcribe segment">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
-                <button class="retranscribe-segment p-1 text-green-500 hover:text-green-700 rounded-full hover:bg-green-100 transition duration-300 ease-in-out" data-index="${index}" title="Re-transcribe segment">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
-                    </svg>
-                </button>
+                <div class="w-20 flex flex-col items-end">
+                    <input type="number" class="segment-start w-full p-1 mb-1 border border-slate-300 rounded-md focus:outline-none focus:ring-0" value="${segment.start.toFixed(2)}" step="0.01" min="0" data-index="${index}">
+                    <input type="number" class="segment-end w-full p-1 mb-1 border border-slate-300 rounded-md focus:outline-none focus:ring-0" value="${segment.end.toFixed(2)}" step="0.01" min="0" data-index="${index}">
+                    <span class="mt-1 text-sm text-slate-600 segment-time text-center w-full">${duration}s</span>
+                </div>
             </div>
         `;
         container.appendChild(div);
     });
-    
+
     container.addEventListener('mousedown', event => {
         const segmentDiv = event.target.closest('.segment-container');
         if (segmentDiv) {
